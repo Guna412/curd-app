6bbe2f396acd01dddeff9deec97da1be092c84b9
whitemouse87 <khaothihcmluutru@gmail.com>
2024-11-21 23:27:13 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -10,3 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+reportWebVitals();
+
+
